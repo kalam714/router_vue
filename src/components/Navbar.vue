f5a0f7fe03bbd5b326a2f393f53e71f6455fc175
kalam ahmed <kalamahmed714@gmail.com>
2020-10-18 22:35:04 +0600

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+<nav>
+    <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+
+    </ul>
+</nav>
+<div>
+    <ul>
+        <li v-for="(id,index) in userIds" :key="index">
+            <router-link :to="{name:'Profile', params:{getid:id}}" >Profile {{id}}</router-link>
+        </li>
+    </ul>
+    <button @click="contactPage">Contact</button>
+    <button @click="aboutPage">About</button>
+</div>
+</div>
+</template>
+<script>
+export default {
+    name:'Navbar',
+    data(){
+        return {
+            userIds:['1','2','3','4',]
+
+        }
+    },
+    methods:{
+        contactPage(){
+            this.$router.push({name:'Contact'})
+        },
+        aboutPage(){
+            this.$router.push({name:'About'})
+        }
+    }
+    
+}
+</script>
+<style>
+nav {
+    text-align: center;
+}
+nav ul{
+    padding:0px;
+}
+nav li{
+    display: inline-block;
+    list-style-type: block;
+    margin: 0px;
+    padding:10px;
+}
+
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+      <h1>Profile Page {{userId}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+      userId :this.$route.params.getid
+    }
+  },
+  methods:{
+     updateId(){
+       this.userId =  this.$route.params.getid
+     }
+  },
+  watch:{
+      $route:'updateId'
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+    color:aqua;
+    text-align: center;
+}
+</style>
